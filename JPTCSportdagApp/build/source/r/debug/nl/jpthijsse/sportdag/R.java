78cd29054f3698565dbb0e1e2c279d421fd646fc
Stefan Breetveld <spiderbiggen@gmail.com>
2013-08-14 00:33:11 +0200

    added test build

--- JPTCSportdagApp/build/source/r/debug/nl/jpthijsse/sportdag/R.java
+++ JPTCSportdagApp/build/source/r/debug/nl/jpthijsse/sportdag/R.java
@@ -9,105 +9,112 @@ package nl.jpthijsse.sportdag;
 
 public final class R {
     public static final class array {
-        /**  From: file:/C:/Users/stefan/AndroidStudioProjects/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values/arrays.xml 
-         */
-        public static final int klas=0x7f040000;
-        public static final int leerlingen_1A=0x7f040001;
-        public static final int leerlingen_1B=0x7f040002;
+        /** From: file:/C:/Users/stefan/Development/JPTSportdagApp/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values/arrays.xml */
+        public static final int klas = 0x7f040000;
+        public static final int leerlingen_1A = 0x7f040001;
+        public static final int leerlingen_1B = 0x7f040002;
     }
+
     public static final class attr {
     }
-    public static final class dimen {
-        /**  From: file:/C:/Users/stefan/AndroidStudioProjects/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values/dimens.xml 
- From: file:/C:/Users/stefan/AndroidStudioProjects/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values-sw720dp-land/dimens.xml 
-         */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
-    }
-    public static final class drawable {
-        public static final int button=0x7f020000;
-        public static final int hardlopen=0x7f020001;
-        public static final int hindernisbaan=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int kogelstoten=0x7f020004;
-        public static final int layout_achtergrond=0x7f020005;
-        public static final int layout_bovenkant=0x7f020006;
-        public static final int layout_onderkant=0x7f020007;
-        public static final int logo_front=0x7f020008;
-        public static final int sprint=0x7f020009;
-        public static final int verspringen=0x7f02000a;
-        public static final int vortexwerpen=0x7f02000b;
-    }
-    public static final class id {
-        public static final int Begeleidingbutton_id=0x7f090003;
-        public static final int Leerlingbutton_id=0x7f090002;
-        public static final int Recordsbutton_id=0x7f090004;
-        public static final int action_settings=0x7f09000c;
-        public static final int bottom_id=0x7f090001;
-        public static final int container=0x7f090005;
-        public static final int klas_spinner=0x7f09000a;
-        public static final int leerling_spinner=0x7f09000b;
-        public static final int pager=0x7f090006;
-        public static final int pager_title_strip=0x7f090007;
-        public static final int section_label=0x7f090008;
-        public static final int sport_image=0x7f090009;
-        public static final int top_id=0x7f090000;
-    }
-    public static final class layout {
-        public static final int activity_begeleiding=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_records=0x7f030002;
-        public static final int activity_splash=0x7f030003;
-        public static final int activity_sport=0x7f030004;
-        public static final int fragment_records_dummy=0x7f030005;
-        public static final int fragment_sport_dummy=0x7f030006;
-    }
-    public static final class menu {
-        public static final int begeleiding=0x7f080000;
-        public static final int main=0x7f080001;
-        public static final int records=0x7f080002;
-        public static final int splash=0x7f080003;
-        public static final int sport=0x7f080004;
-    }
-    public static final class string {
-        /**  From: file:/C:/Users/stefan/AndroidStudioProjects/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values/strings.xml 
-         */
-        public static final int action_settings=0x7f060000;
-        public static final int app_name=0x7f060001;
-        public static final int begeleidingbutton=0x7f060002;
-        public static final int bottomDesc=0x7f060003;
-        public static final int hardlopen=0x7f060004;
-        public static final int hindernisbaan=0x7f060005;
-        public static final int kieslln=0x7f060006;
-        public static final int kogel=0x7f060007;
-        public static final int leerlingbutton=0x7f060008;
-        public static final int recordbutton=0x7f060009;
-        public static final int sport_image_string=0x7f06000a;
-        public static final int sprint=0x7f06000b;
-        public static final int title_activity_kies_sport=0x7f06000c;
-        public static final int title_activity_sport=0x7f06000d;
-        public static final int title_section1=0x7f06000e;
-        public static final int title_section2=0x7f06000f;
-        public static final int title_section3=0x7f060010;
-        public static final int topbarDesc=0x7f060011;
-        public static final int verspringen=0x7f060012;
-        public static final int vortex=0x7f060013;
-    }
-    public static final class style {
-        /**  From: file:/C:/Users/stefan/AndroidStudioProjects/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values/styles.xml 
 
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
- From: file:/C:/Users/stefan/AndroidStudioProjects/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values-v11/styles.xml 
- API 11 theme customizations can go here. 
- From: file:/C:/Users/stefan/AndroidStudioProjects/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values-v14/styles.xml 
- API 14 theme customizations can go here. 
+    public static final class dimen {
+        /**
+         * From: file:/C:/Users/stefan/Development/JPTSportdagApp/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values/dimens.xml
+         * From: file:/C:/Users/stefan/Development/JPTSportdagApp/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values-sw720dp-land/dimens.xml
          */
-        public static final int AppBaseTheme=0x7f070000;
-        /**  All customizations that are NOT specific to a particular API-level can go here. 
+        public static final int activity_horizontal_margin = 0x7f050000;
+        public static final int activity_vertical_margin = 0x7f050001;
+    }
+
+    public static final class drawable {
+        public static final int button = 0x7f020000;
+        public static final int hardlopen = 0x7f020001;
+        public static final int hindernisbaan = 0x7f020002;
+        public static final int ic_launcher = 0x7f020003;
+        public static final int kogelstoten = 0x7f020004;
+        public static final int layout_achtergrond = 0x7f020005;
+        public static final int layout_bovenkant = 0x7f020006;
+        public static final int layout_onderkant = 0x7f020007;
+        public static final int logo_front = 0x7f020008;
+        public static final int sprint = 0x7f020009;
+        public static final int verspringen = 0x7f02000a;
+        public static final int vortexwerpen = 0x7f02000b;
+    }
+
+    public static final class id {
+        public static final int Begeleidingbutton_id = 0x7f090003;
+        public static final int Leerlingbutton_id = 0x7f090002;
+        public static final int Recordsbutton_id = 0x7f090004;
+        public static final int action_settings = 0x7f09000c;
+        public static final int bottom_id = 0x7f090001;
+        public static final int container = 0x7f090005;
+        public static final int klas_spinner = 0x7f09000a;
+        public static final int leerling_spinner = 0x7f09000b;
+        public static final int pager = 0x7f090006;
+        public static final int pager_title_strip = 0x7f090007;
+        public static final int section_label = 0x7f090008;
+        public static final int sport_image = 0x7f090009;
+        public static final int top_id = 0x7f090000;
+    }
+
+    public static final class layout {
+        public static final int activity_begeleiding = 0x7f030000;
+        public static final int activity_main = 0x7f030001;
+        public static final int activity_records = 0x7f030002;
+        public static final int activity_splash = 0x7f030003;
+        public static final int activity_sport = 0x7f030004;
+        public static final int fragment_records_dummy = 0x7f030005;
+        public static final int fragment_sport_dummy = 0x7f030006;
+    }
+
+    public static final class menu {
+        public static final int begeleiding = 0x7f080000;
+        public static final int main = 0x7f080001;
+        public static final int records = 0x7f080002;
+        public static final int splash = 0x7f080003;
+        public static final int sport = 0x7f080004;
+    }
+
+    public static final class string {
+        /** From: file:/C:/Users/stefan/Development/JPTSportdagApp/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values/strings.xml */
+        public static final int action_settings = 0x7f060000;
+        public static final int app_name = 0x7f060001;
+        public static final int begeleidingbutton = 0x7f060002;
+        public static final int bottomDesc = 0x7f060003;
+        public static final int hardlopen = 0x7f060004;
+        public static final int hindernisbaan = 0x7f060005;
+        public static final int kieslln = 0x7f060006;
+        public static final int kogel = 0x7f060007;
+        public static final int leerlingbutton = 0x7f060008;
+        public static final int recordbutton = 0x7f060009;
+        public static final int sport_image_string = 0x7f06000a;
+        public static final int sprint = 0x7f06000b;
+        public static final int title_activity_kies_sport = 0x7f06000c;
+        public static final int title_activity_sport = 0x7f06000d;
+        public static final int title_section1 = 0x7f06000e;
+        public static final int title_section2 = 0x7f06000f;
+        public static final int title_section3 = 0x7f060010;
+        public static final int topbarDesc = 0x7f060011;
+        public static final int verspringen = 0x7f060012;
+        public static final int vortex = 0x7f060013;
+    }
+
+    public static final class style {
+        /**
+         * From: file:/C:/Users/stefan/Development/JPTSportdagApp/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values/styles.xml
+         * <p/>
+         * Theme customizations available in newer API levels can go in
+         * res/values-vXX/styles.xml, while customizations related to
+         * backward-compatibility can go here.
+         * <p/>
+         * From: file:/C:/Users/stefan/Development/JPTSportdagApp/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values-v11/styles.xml
+         * API 11 theme customizations can go here.
+         * From: file:/C:/Users/stefan/Development/JPTSportdagApp/JPTCSportdagAppProject/JPTCSportdagApp/src/main/res/values-v14/styles.xml
+         * API 14 theme customizations can go here.
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppBaseTheme = 0x7f070000;
+        /** All customizations that are NOT specific to a particular API-level can go here. */
+        public static final int AppTheme = 0x7f070001;
     }
 }
